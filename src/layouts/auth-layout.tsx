0e6079ae22ffc3d44e9b8aea00a67300d6a6c126
Aquib349 <aquib_m@optimusbt.net>
2025-02-12 16:28:31 +0530

    code : bug fixes + responsivness

--- src/layouts/auth-layout.tsx
+++ src/layouts/auth-layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const AuthLayout = () => {
   return (
     <section className="auth-layout min-h-screen flex justify-center items-center custom-bg">
-      <div className="w-full sm:w-3/4 md:w-2/3 lg:w-1/3 xl:w-1/4 rounded-lg shadow-lg">
+      <div className="w-96 mx-auto">
         <Outlet />
       </div>
     </section>
